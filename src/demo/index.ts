7092be976068eb889607feed68906177e5d8b5d8
Smile <cxz0917001997@gmail.com>
2020-03-05 09:44:07 +0800

    generate file

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -3,8 +3,13 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function demo(_options: any): Rule {
+export function demo(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create('demo.js', `console.log('${_options.name}');`);
     return tree;
   };
+}
+
+interface Schema {
+  name: string;
 }
